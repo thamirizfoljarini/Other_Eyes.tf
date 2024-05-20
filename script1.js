b3d2c3345a38306fb326c746ebcb138bf9181a22
ThamizSF <61845672+thamirizfoljarini@users.noreply.github.com>
2024-05-19 22:26:04 -0300

    Create script1.js
    script da página 1

---
+++ script1.js
@@ -0,0 +1,15 @@
+<script>
+        // Função para redirecionar o usuário para diferentes páginas ao clicar nos botões
+        document.getElementById('btn1').addEventListener('click', function() {
+            window.location.href = 'pagina1.html';
+        });
+
+        document.getElementById('btn2').addEventListener('click', function() {
+            window.location.href = 'pagina2.html';
+        });
+
+        document.getElementById('btn3').addEventListener('click', function() {
+            window.location.href = 'pagina3.html';
+        });
+
+    </script>
